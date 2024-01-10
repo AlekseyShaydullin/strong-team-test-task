e50b71265b8620f7d245bd38981cc18eb26091dc
Aleksey Shaydullin <101606512+AlekseyShaydullin@users.noreply.github.com>
2024-01-10 19:57:51 +0300

    feat: added empty component

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -3,6 +3,7 @@ import Task from '../task/task';
 import Form from '../form/form';
 import './App.css';
 import Counters from '../counters/counters';
+import Empty from '../empty/empty';
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -12,6 +13,7 @@ function App() {
       <Form />
       <Counters />
       <Task />
+      <Empty />
     </>
   );
 }
---
+++ src/components/empty/empty.tsx
@@ -0,0 +1,22 @@
+import { FC } from 'react';
+import style from './empty.module.scss';
+import Icon from '../ui/icon/icon';
+import Typography from '../ui/typography/typography';
+
+const Empty: FC = (): JSX.Element => {
+  return (
+    <div className={style.empty__wrapper}>
+      <Icon name="clipboard" isColored extraClass={style.icon} />
+      <div className={style.title__wrapper}>
+        <Typography tag="span" className={style.title}>
+          У вас еще нет зарегистрированных задач
+        </Typography>
+        <Typography tag="span">
+          Создавайте задачи и организуйте свои дела
+        </Typography>
+      </div>
+    </div>
+  );
+};
+
+export default Empty;
